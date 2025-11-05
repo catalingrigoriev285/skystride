be23b377817be3d4fbc18342e122fc6e4451572d
Cătălin Grigoriev <catalingrigoriev50@gmail.com>
2025-11-05 15:35:41 +0200

    Add camera and grid rendering to engine
    Introduces a Camera class for 3D navigation and a Grid class for scene visualization. Updates Engine to use the new Camera for view/projection matrices and input handling, and renders the grid in the main scene. Also adds necessary project and solution files for build configuration.

--- vendor/Camera.cs
+++ vendor/Camera.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using OpenTK;
+using OpenTK.Input;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +11,99 @@ namespace skystride.vendor
 {
     internal class Camera
     {
+        // instances
+        public Vector3 position { get; private set; }
+        public Vector3 front { get; private set; } = -Vector3.UnitZ;
+        public Vector3 up { get; private set; } = Vector3.UnitY;
+        public Vector3 right { get; private set; } = Vector3.UnitX;
 
+        // euler rotations
+        private float yaw = -90.0f;
+        private float pitch = 0.0f;
+
+        private float speed = 5f;
+
+        private float sensitivity = 0.2f;
+
+        private float fov = 60.0f;
+        private float aspectRatio;
+
+        private bool firstMoveState = true;
+        private Vector2 latestMousePosition;
+
+        public Camera(Vector3 _position, float _aspectRatio)
+        {
+            this.position = _position;
+            this.aspectRatio = _aspectRatio;
+
+            this.UpdateVectors();
+        }
+
+        public Matrix4 GetViewMatrix()
+        {
+            return Matrix4.LookAt(this.position, this.position + this.front, this.up);
+        }
+
+        public Matrix4 GetProjectionMatrix()
+        {
+            return Matrix4.CreatePerspectiveFieldOfView(MathHelper.DegreesToRadians(this.fov), this.aspectRatio, 0.1f, 1000f);
+        }
+
+        public void UpdateKeyboardState(KeyboardState _currentKeyboardState, float deltaTime)
+        {
+            float velocity = speed * deltaTime;
+
+            if (_currentKeyboardState.IsKeyDown(Key.W))
+                this.position += this.front * velocity;
+            if (_currentKeyboardState.IsKeyDown(Key.S))
+                this.position -= this.front * velocity;
+            if (_currentKeyboardState.IsKeyDown(Key.A))
+                this.position -= this.right * velocity;
+            if (_currentKeyboardState.IsKeyDown(Key.D))
+                this.position += this.right * velocity;
+            if (_currentKeyboardState.IsKeyDown(Key.Space))
+                this.position += this.up * velocity;
+            if (_currentKeyboardState.IsKeyDown(Key.ControlLeft))
+                this.position -= this.up * velocity;
+        }
+
+        public void UpdateMouseState(MouseState _currentMouseState)
+        {
+            Vector2 _mousePosition = new Vector2(_currentMouseState.X, _currentMouseState.Y);
+
+            if (this.firstMoveState)
+            {
+                this.latestMousePosition = _mousePosition;
+                this.firstMoveState = false;
+            }
+
+            float deltaX = _currentMouseState.X - this.latestMousePosition.X;
+            float deltaY = _currentMouseState.Y - this.latestMousePosition.Y;
+            this.latestMousePosition = _mousePosition;
+
+            this.yaw += deltaX * this.sensitivity;
+            this.pitch -= deltaY * this.sensitivity;
+
+            pitch = MathHelper.Clamp(this.pitch, -89f, 89f);
+
+            UpdateVectors();
+        }
+
+        private void UpdateVectors()
+        {
+            Vector3 _frontState;
+            _frontState.X = (float)(System.Math.Cos(MathHelper.DegreesToRadians(this.yaw)) * System.Math.Cos(MathHelper.DegreesToRadians(this.pitch)));
+            _frontState.Y = (float)System.Math.Sin(MathHelper.DegreesToRadians(this.pitch));
+            _frontState.Z = (float)(System.Math.Sin(MathHelper.DegreesToRadians(this.yaw)) * System.Math.Cos(MathHelper.DegreesToRadians(this.pitch)));
+
+            this.front = Vector3.Normalize(_frontState);
+            this.right = Vector3.Normalize(Vector3.Cross(this.front, Vector3.UnitY));
+            this.up = Vector3.Normalize(Vector3.Cross(this.right, this.front));
+        }
+
+        public void Resize(float _aspect_ratio)
+        {
+            this.aspectRatio = _aspect_ratio;
+        }
     }
 }
--- vendor/Engine.cs
+++ vendor/Engine.cs
@@ -8,6 +8,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using skystride.objects.templates;
 
 namespace skystride.vendor
 {
@@ -16,6 +17,15 @@ namespace skystride.vendor
         // inits
         private KeyboardState currentKeyboardState, previousKeyboardState;
         private MouseState currentMouseState, previousMouseState;
+
+        // main instances
+        Camera camera;
+        private bool firstCameraMove = true;
+        private bool isMouseCentered = false;
+        private Vector2 latestMousePosition;
+
+        // objects instances
+        Grid grid = new Grid();
 
         // init engine window
         public Engine() : base(800, 600, new GraphicsMode(32, 24, 0, 8))
@@ -34,6 +44,13 @@ namespace skystride.vendor
             GL.DepthFunc(DepthFunction.Less);
 
             GL.Hint(HintTarget.PolygonSmoothHint, HintMode.Nicest);
+            GL.Hint(HintTarget.LineSmoothHint, HintMode.Nicest);
+            GL.Enable(EnableCap.LineSmooth);
+
+            camera = new Camera(new Vector3(0, 0, 3), Width / (float)Height);
+
+            CursorVisible = false;
+            this.isMouseCentered = true;
         }
 
         // on resize event
@@ -43,15 +60,13 @@ namespace skystride.vendor
 
             GL.Viewport(0, 0, this.Width, this.Height);
 
-            // set perspective projection
-            Matrix4 perspective = Matrix4.CreatePerspectiveFieldOfView(MathHelper.PiOver4, (float)Width / (float)Height, 1, 256);
-            GL.MatrixMode(MatrixMode.Projection);
-            GL.LoadMatrix(ref perspective);
+            camera.Resize(Width / (float)Height);
 
-            // default camera template
-            Matrix4 camera = Matrix4.LookAt(30, 30, 30, 0, 0, 0, 0, 1, 0);
+            // Update projection on resize
+            var projection = camera.GetProjectionMatrix();
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.LoadMatrix(ref projection);
             GL.MatrixMode(MatrixMode.Modelview);
-            GL.LoadMatrix(ref camera);
         }
 
         // on update frame event
@@ -60,13 +75,29 @@ namespace skystride.vendor
             base.OnUpdateFrame(e);
 
             this.currentKeyboardState = Keyboard.GetState();
+            this.currentMouseState = Mouse.GetState();
 
             if (currentKeyboardState[Key.Escape])
             {
-                Exit();
+                CursorVisible = true;
+                this.isMouseCentered = false;
+            }
+
+            if(currentMouseState.LeftButton == ButtonState.Pressed)
+            {
+                CursorVisible = false;
+                this.isMouseCentered = true;
+            }
+
+            if (!CursorVisible && this.isMouseCentered && Focused)
+            {
+                Mouse.SetPosition(X + Width / 2f, Y + Height / 2f);
+                camera.UpdateMouseState(this.currentMouseState);
             }
 
             this.previousKeyboardState = this.currentKeyboardState;
+
+            camera.UpdateKeyboardState(this.currentKeyboardState, (float)e.Time);
         }
 
         // on render frame event
@@ -74,8 +105,15 @@ namespace skystride.vendor
         {
             base.OnRenderFrame(e);
 
-            GL.Clear(ClearBufferMask.ColorBufferBit);
-            GL.Clear(ClearBufferMask.DepthBufferBit);
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+            Matrix4 viewMatrix = camera.GetViewMatrix();
+            GL.MatrixMode(MatrixMode.Modelview);
+            GL.LoadMatrix(ref viewMatrix);
+
+            /* RENDER AREA */
+            grid.Render();
+            /* END RENDER AREA */
 
             SwapBuffers();
         }
