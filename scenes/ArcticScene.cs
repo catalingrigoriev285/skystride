2a51486f39d0332a73d66099ebd3326a68b233b4
Cătălin Grigoriev <catalingrigoriev50@gmail.com>
2025-11-08 18:54:59 +0200

    Update ArcticScene.cs

--- scenes/ArcticScene.cs
+++ scenes/ArcticScene.cs
@@ -21,21 +21,6 @@ namespace skystride.scenes
             platform.SetTextureScale(5, 5);
             AddEntity(platform);
 
-            Plane pereteA = new Plane(new Vector3(20f, 5f, 0f), 1f, 70f, 10f, Color.LightGray, new Vector3(-1f, 0f, 0f));
-            pereteA.SetTexture("assets/textures/snow.png");
-            pereteA.SetTextureScale(5, 5);
-            AddEntity(pereteA);
-
-            Plane pereteC = new Plane(new Vector3(-37f, 5f, 35.5f), 150f, 1f, 10f, Color.LightGray, new Vector3(0f, 0f, -1f));
-            pereteC.SetTexture("assets/textures/snow.png");
-            pereteC.SetTextureScale(5, 5);
-            AddEntity(pereteC);
-
-            Plane pereteD = new Plane(new Vector3(-37f, 0f, -35.5f), 150f, 1f, 20f, Color.LightGray, new Vector3(0f, 0f, -1f));
-            pereteD.SetTexture("assets/textures/snow.png");
-            pereteD.SetTextureScale(5, 5);
-            AddEntity(pereteD);
-
             AddEntity(new ModelEntity(
                 new Model("/assets/models/copac1.obj", "/assets/models/copac1.png"),
                 new Vector3(12f, 6f, 22f), 1f, 0f, 0f, 0f));
