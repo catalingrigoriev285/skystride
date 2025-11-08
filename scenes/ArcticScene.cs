5d57f9cf209bb1f973458b4fb38b73aeb936b36b
Cătălin Grigoriev <catalingrigoriev50@gmail.com>
2025-11-08 17:18:33 +0200

    Update ArcticScene.cs

--- scenes/ArcticScene.cs
+++ scenes/ArcticScene.cs
@@ -7,7 +7,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using skystride.shaders; // added for Snow
+using skystride.shaders;
 
 namespace skystride.scenes
 {
@@ -15,17 +15,11 @@ namespace skystride.scenes
     {
         public ArcticScene()
         {
-            AddEntity(new Grid());
+            Plane platform = new Plane(new Vector3(0f, 0f, 0f), 70f, 70f, 1f, Color.IndianRed, new Vector3(0f, 1f, 0f));
+            platform.SetTexture("assets/textures/snow.png");
+            AddEntity(platform);
 
-            Plane spawnPlane = new Plane(new Vector3(0f, 0f, 0f), 20f, 10f, 1f, Color.IndianRed, new Vector3(0f, 1f, 0f));
-            spawnPlane.SetTexture("assets/textures/snow.png");
-            AddEntity(spawnPlane);
 
-            Plane spawnPlane2 = new Plane(new Vector3(0f, 0f, 20f), 20f, 10f, 1f, Color.IndianRed, new Vector3(0f, 1f, 0f));
-            spawnPlane2.SetTexture("assets/textures/snow.png");
-            AddEntity(spawnPlane2);
-
-            // Add snow particle system (tuned for arctic environment)
             AddEntity(new Snow(count: 7500, areaSize: 120f, spawnHeight: 50f, groundY: 0f, minSpeed: 1.5f, maxSpeed: 4.5f));
         }
     }
