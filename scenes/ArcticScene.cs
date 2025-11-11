e8adfea15a23c31b0487c3b5f213c9096dd23e5d
Cătălin Grigoriev <catalingrigoriev50@gmail.com>
2025-11-11 16:50:22 +0200

    Add new entities and camera reset to ArcticScene
    Added several new ModelEntity instances to the ArcticScene, adjusted positions of existing entities, and implemented camera position reset functionality when the 'R' key is pressed.

--- scenes/ArcticScene.cs
+++ scenes/ArcticScene.cs
@@ -34,22 +34,39 @@ namespace skystride.scenes
                 new Vector3(-13f, 6f, 14f), 1f, 0f, 0f, 0f));
 
             AddEntity(new ModelEntity(
-                new Model("/assets/models/iashik.obj", "/assets/models/iashik.jpg"),
-                new Vector3(-27f, -1f, 0f), 5f, 0f, 90f, -360f));
-
-            AddEntity(new ModelEntity(
-                new Model("/assets/models/iashik.obj", "/assets/models/iashik.jpg"),
-                new Vector3(-37f, 0f, 4f), 5f, 0f, 90f, -360f));
-
-            AddEntity(new ModelEntity(
                 new Model("/assets/models/copac1.obj", "/assets/models/copac1.png"),
                 new Vector3(-4f, 6f, -22f), 1f, 0f, 0f, 0f));
+
+            AddEntity(new ModelEntity(
+                new Model("/assets/models/iashik.obj", "/assets/models/iashik.jpg"),
+                new Vector3(-27f, -2f, 0f), 5f, 0f, 90f, -360f));
+
+            AddEntity(new ModelEntity(
+                new Model("/assets/models/iashik.obj", "/assets/models/iashik.jpg"),
+                new Vector3(-37f, -2f, 4f), 5f, 0f, 90f, -360f));
+
+            AddEntity(new ModelEntity(
+                new Model("/assets/models/iashik.obj", "/assets/models/iashik.jpg"),
+                new Vector3(-47f, -2f, 0f), 5f, 0f, 90f, -360f));
+
+            AddEntity(new ModelEntity(
+                new Model("/assets/models/iashik.obj", "/assets/models/iashik.jpg"),
+                new Vector3(-57f, -2f, 4f), 5f, 0f, 90f, -360f));
+
+            AddEntity(new ModelEntity(
+                new Model("/assets/models/iashik.obj", "/assets/models/iashik.jpg"),
+                new Vector3(-67f, -2f, 2f), 5f, 0f, 90f, -360f));
 
             AddEntity(new Snow(count: 7500, areaSize: 120f, spawnHeight: 50f, groundY: 0f, minSpeed: 1.5f, maxSpeed: 4.5f));
         }
 
         public override void Update(float dt, Camera camera, KeyboardState currentKeyboard, KeyboardState previousKeyboard, MouseState currentMouse, MouseState previousMouse)
         {
+            if (currentKeyboard[Key.R])
+            {
+                camera.SetPosition(new Vector3(0f, 5f, 10f));
+            }
+
             camera.ResolveCollisions(Colliders);
         }
     }
