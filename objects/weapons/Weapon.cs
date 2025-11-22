e585c3d0b8c5a9259b534c839d3d35431a1f7c6c
Cătălin Grigoriev <catalingrigoriev50@gmail.com>
2025-11-22 11:55:42 +0200

    Add Weapon base class and Glock pistol implementation
    Introduces the Weapon base class with properties for name, ammo, and damage. Adds a Glock pistol subclass with a specific model and default attributes.

---
+++ objects/weapons/Weapon.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace skystride.objects.weapons
+{
+    internal class Weapon
+    {
+        protected int ammo, damage;
+        protected string name;
+
+        public Weapon(string name, int ammo, int damage)
+        {
+            this.name = name;
+            this.ammo = ammo;
+            this.damage = damage;
+        }
+    }
+}
---
+++ objects/weapons/pistols/glock.cs
@@ -0,0 +1,18 @@
+﻿using skystride.vendor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace skystride.objects.weapons.pistols
+{
+    internal class glock : Weapon
+    {
+        private Model model;
+        public glock() : base("Glock", 17, 25)
+        {
+            this.model = new Model("assets/models/weapons/glock.obj");
+        }
+    }
+}
